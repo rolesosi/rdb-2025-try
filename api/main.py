afdc93eaa38564be26f6881607437c60971ae570
Roberto Leandro de Souza Silva <robertol.silva@prefeitura.rio>
2025-07-31 19:10:18 -0300

    feat: melhoria de processamento de pagamento com retorno de correlation ID e summary; adicionadas variaveis de ambiente para processor URLs

--- api/main.py
+++ api/main.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, Request
+from fastapi import FastAPI, Request, Query
 import redis.asyncio as aioredis
 import uuid, json
 
@@ -13,11 +13,31 @@ async def startup():
 @app.post("/payments")
 async def create_payment(req: Request):
     data = await req.json()
-    payment_id = str(uuid.uuid4())
-    await redis.rpush("payment_queue", json.dumps({ "id": payment_id, "amount": data["amount"] }))
-    return {"status": "queued", "id": payment_id}
+    correlation_id = data.get("correlationId")
+    amount = data.get("amount")
+    await redis.rpush("payment_queue", json.dumps({ 
+        "correlationId": correlation_id,
+        "amount": amount
+    }))
+    return {"status": "queued",  "correlationId": correlation_id}
 
 @app.get("/payments-summary")
-async def summary():
-    summary = await redis.hgetall("summary")
-    return summary or {"success": 0, "fallback": 0}
+async def summary(
+    from_: str = Query(None, alias="from"),
+    to: str = Query(None)
+):
+    # Busca os dados do resumo
+    default_requests = int(await redis.hget("summary", "success") or 0)
+    fallback_requests = int(await redis.hget("summary", "fallback") or 0)
+    default_amount = float(await redis.hget("summary", "success_amount") or 0)
+    fallback_amount = float(await redis.hget("summary", "fallback_amount") or 0)
+    return {
+        "default": {
+            "totalRequests": default_requests,
+            "totalAmount": default_amount
+        },
+        "fallback": {
+            "totalRequests": fallback_requests,
+            "totalAmount": fallback_amount
+        }
+    }
--- worker/worker.py
+++ worker/worker.py
@@ -1,33 +1,46 @@
 import asyncio, aiohttp, json
+import sys
 import aioredis
+import os
 
+DEFAULT_PROCESSOR_URL = os.getenv("DEFAULT_PROCESSOR_URL")
+FALLBACK_PROCESSOR_URL = os.getenv("FALLBACK_PROCESSOR_URL")
+
+if not DEFAULT_PROCESSOR_URL :
+    print("Erro: A variável de ambiente DEFAULT_PROCESSOR_URL deve estar definida.")
+    sys.exit(1)
+if not FALLBACK_PROCESSOR_URL:    
+    print("Erro: A variável de ambiente FALLBACK_PROCESSOR_URL  deve estar definida.")
+    sys.exit(1)
+    
 async def process():
     redis = await aioredis.from_url("redis://redis", decode_responses=True)
-    session = aiohttp.ClientSession()
+    async with aiohttp.ClientSession() as session:
+        while True:
+            task = await redis.lpop("payment_queue")
+            if not task:
+                await asyncio.sleep(0.01)
+                continue
 
-    while True:
-        task = await redis.lpop("payment_queue")
-        if not task:
-            await asyncio.sleep(0.01)
-            continue
+            payment = json.loads(task)
+            processor_url = await choose_processor(redis)
 
-        payment = json.loads(task)
-        processor_url = await choose_processor(redis)
+            try:
+                async with session.post(f"{processor_url}/payments", json=payment, timeout=0.005) as resp:
+                    if resp.status == 200:
+                        await redis.hincrby("summary", "success", 1)
+                        await redis.hincrbyfloat("summary", "success_amount", float(payment["amount"]))
+                        continue
+            except:
+                pass
 
-        try:
-            async with session.post(f"{processor_url}/payments", json=payment, timeout=0.005) as resp:
-                if resp.status == 200:
-                    await redis.hincrby("summary", "success", 1)
-                    continue
-        except:
-            pass
-
-        await redis.hincrby("summary", "fallback", 1)
+            await redis.hincrby("summary", "fallback", 1)
+            await redis.hincrbyfloat("summary", "fallback_amount", float(payment["amount"]))
 
 async def choose_processor(redis):
     a_health = await redis.get("procA_health") or "ok"
     if a_health == "ok":
-        return "http://payment-processor-a"
-    return "http://payment-processor-b"
+        return DEFAULT_PROCESSOR_URL
+    return FALLBACK_PROCESSOR_URL
 
 asyncio.run(process())
